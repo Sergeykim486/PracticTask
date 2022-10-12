6863dd517a0728384aabc24f7b0327583d775e3c
Sergey Kim <Sergeykim486@gmail.com>
2022-10-13 00:28:42 +0500

    Created menu and interface

--- Program.cs
+++ Program.cs
@@ -1,1 +1,229 @@
-﻿
+﻿// ═══════════════════════════════ Главное меню и основные функции ═══════════════════════════════
+
+int choice = 1; // ТЕКУЩИЙ ВЫБРАННЫЙ ЭЛЕМЕНТ
+// Список меню
+string[] ListMenu = {
+    "                                                                          ",
+    "  ВЫХОД ИЗ ПРОГРАММЫ                                                      "};
+Console.ForegroundColor = ConsoleColor.White;
+Console.BackgroundColor = ConsoleColor.Black;
+void menu() // Функция выводит главное меню на экран консоли
+{
+    int CurrentLine = 1;
+    Console.ForegroundColor = ConsoleColor.White;
+    Console.BackgroundColor = ConsoleColor.Black;
+    Console.WriteLine(choice);
+    int i = 0;
+    Console.Clear();
+    Console.ForegroundColor = ConsoleColor.White;
+    Console.WriteLine("═══════════════════════  Г Л А В Н О Е   М Е Н Ю  ════════════════════════\n");
+    Console.ForegroundColor = ConsoleColor.White;
+    while (i < ListMenu.Length)
+    {
+        CurrentLine = i + 1;
+        if (choice == CurrentLine)
+        {
+            if (i == ListMenu.Length - 1)
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.BackgroundColor = ConsoleColor.DarkRed;
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Black;
+                Console.BackgroundColor = ConsoleColor.Yellow;
+            }
+            Console.Write($"{ListMenu[i].ToUpper()}\n");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.BackgroundColor = ConsoleColor.Black;
+        }
+        else
+        {
+            if (i == ListMenu.Length - 1)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.BackgroundColor = ConsoleColor.Black;
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.BackgroundColor = ConsoleColor.Black;
+            }
+            Console.Write($"{ListMenu[i]}\n");
+        }
+        i++;
+    }
+    Console.ForegroundColor = ConsoleColor.DarkCyan;
+    Console.WriteLine("\n" +
+    "╔════════════════════════════════════════════════════════╤═══════════════╗\n" +
+    "║  Используйте [стрелки] для навигации.                  │   ▲ : Вверх   ║\n" +
+    "║  [ENTER] - Выбор выделенного пункта                    │   ▼ : Вниз    ║\n" +
+    "╚════════════════════════════════════════════════════════╧═══════════════╝\n");
+    Console.BackgroundColor = ConsoleColor.Black;
+}
+int GetNum() // Запрос на ввод целого числа
+{
+    Console.ForegroundColor = ConsoleColor.White;
+    Console.Write("_______________________________________________\n" +
+    "Введите целое число... ");
+    int result1 = Convert.ToInt32(Console.ReadLine());
+    return result1;
+}
+string GetString() // Запрос на ввод целого числа
+{
+    Console.ForegroundColor = ConsoleColor.White;
+    Console.Write("_______________________________________________\n" +
+    "Введите целое число... ");
+    string result1 = Convert.ToString(Console.ReadLine());
+    return result1;
+}
+void ErrorCatch() // Функция обрабатывает ошибку, выдает сообщение и запускает программу с начала
+{
+    Console.Clear();
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine("" +
+    "  ╔═ О Ш И Б К А  В В О Д А ════════════════════════════╗\n" +
+    "  ║  Возможно вы ввели не верные данные.                ║\n" +
+    "  ║  Программа будет перезапущена...                    ║\n" +
+    "  ╚═════════════════════════════════════════════════════╝\n");
+    Console.ForegroundColor = ConsoleColor.White;
+    pause();
+}
+void pause() // Функция паузы, для чтения результатов
+{
+    Console.ForegroundColor = ConsoleColor.Cyan;
+    Console.WriteLine("" +
+    "  ╔═════════════════════════════════════════════════════╗\n" +
+    "  ║  ЧТОБЫ ПРОДОЛЖИТЬ НАЖМИТЕ ЛЮБУЮ КЛАВИШУ...          ║\n" +
+    "  ╚═════════════════════════════════════════════════════╝\n");
+    ConsoleKeyInfo key;
+    key = Console.ReadKey();
+    try
+    {
+        main();
+    }
+    catch
+    {
+        ErrorCatch();
+    }
+}
+void ext() // Функция задает пользователю вопрос хочет ли он выйти из программы и закрывает ее если ответ да
+{
+AskAgayn:
+    Console.Clear();
+    Console.WriteLine();
+    Console.WriteLine();
+    Console.ForegroundColor = ConsoleColor.White;
+    Console.BackgroundColor = ConsoleColor.DarkRed;
+    Console.WriteLine("  ╔═ В Ы Х О Д   И З   П Р О Г Р А М М Ы ═══════════════╗  \n" +
+    "  ║      Вы уверены что хотите закрыть программу?       ║  \n" +
+    "  ║              [ENTER] Да     [ESC] Нет               ║  \n" +
+    "  ╚═════════════════════════════════════════════════════╝  \n");
+    Console.ForegroundColor = ConsoleColor.White;
+    Console.BackgroundColor = ConsoleColor.Black;
+    ConsoleKeyInfo key;
+    key = Console.ReadKey();
+    if (key.Key != ConsoleKey.Enter && key.Key != ConsoleKey.Escape)
+    {
+        Console.Clear();
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("" +
+        "  ╔═ О Ш И Б К А  В В О Д А ════════════════════════════╗\n" +
+        "  ║  Вам нужно нажать [ENTER] или [ESC].                ║\n" +
+        "  ╚═════════════════════════════════════════════════════╝\n");
+        Console.ForegroundColor = ConsoleColor.Cyan;
+        Console.WriteLine("" +
+        "  ╔═════════════════════════════════════════════════════╗\n" +
+        "  ║  ЧТОБЫ ПРОДОЛЖИТЬ НАЖМИТЕ ЛЮБУЮ КЛАВИШУ...          ║\n" +
+        "  ╚═════════════════════════════════════════════════════╝\n");
+        ConsoleKeyInfo key1;
+        key1 = Console.ReadKey();
+        Console.ForegroundColor = ConsoleColor.White;
+        goto AskAgayn;
+    }
+    else if (key.Key == ConsoleKey.Escape) main();
+    else if (key.Key == ConsoleKey.Enter)
+    {
+        Console.Clear();
+        System.Environment.Exit(0);
+    }
+    else main();
+}
+
+// ═══════════════════════════════ Обработка выбранного элемента в меню ═══════════════════════════════
+
+// ВЫПОЛНЕНИЕ ПРОГРАММЫ И ОТСЛЕЖИВАНИЕ НАЖАТИЯ КЛАВИШ
+void main()
+{
+restart:
+    menu();
+    ConsoleKeyInfo key;
+    key = Console.ReadKey();
+    if (key.Key == ConsoleKey.UpArrow)
+    {
+        if (choice <= ListMenu.Length)
+        {
+            if (choice == 1)
+            {
+                choice = ListMenu.Length;
+                goto restart;
+            }
+            else
+            {
+                choice = choice - 1;
+                goto restart;
+            }
+        }
+        else goto restart;
+    }
+    else if (key.Key == ConsoleKey.DownArrow)
+    {
+        if (choice >= 1)
+        {
+            if (choice == ListMenu.Length)
+            {
+                choice = 1;
+                goto restart;
+            }
+            else
+            {
+                choice = choice + 1;
+                goto restart;
+            }
+        }
+        else goto restart;
+    }
+    else if (key.Key == ConsoleKey.Enter)
+    {
+        // Здесь объявляются функции которые программа выполнит при выборе того или иного пункта меню
+        switch (choice)
+        {
+            case 1:
+                Console.Clear();
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("Задача:");
+                // Вывод результата на экран...
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.Write("\n═══════════════════════════════════════════════════════════════════════════════════════════\n");
+                Console.Write("═══════════════════════════════════════════════════════════════════════════════════════════\n\n");
+                pause();
+                goto restart;
+            case 2:
+                ext();
+                goto restart;
+        }
+    }
+    else goto restart;
+}
+
+// ═══════════════════════════════ Запуск обработки меню и выбранного элемента ═══════════════════════════════
+
+try
+{
+    main();
+}
+
+catch
+{
+    ErrorCatch();
+}
